59f83bc49ad4d6a636db62f4a8a448526de98559
FathiFarhat <thif215@gmail.com>
2023-11-13 16:24:10 +0700

    refactor: phone number

--- component/form.jsx
+++ component/form.jsx
@@ -25,6 +25,14 @@ const Index = () => {
       [name]: files ? files[0] : value,
     }));
   };
+  let formatPhoneNumber = (event) => {
+    const input = event.target;
+    const numericValue = input.value.replace(/\D/g, '');
+
+    if (!numericValue.startsWith('62')) {
+      input.value = '+62' + numericValue;
+    }
+  };
 
   const handleSubmit = async(e) => {
     e.preventDefault();
@@ -71,7 +79,7 @@ const Index = () => {
         <div className="mb-4">
           <div className="relative z-0 w-full mb-6 group">
             <input type="text" name="name" id="floating_email" onChange={handleChange} value={formData.name}className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-            <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name :</label>
+            <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name </label>
           </div>
         </div>
 
@@ -86,7 +94,7 @@ const Index = () => {
 
         <div className="mb-4">
         <div className="relative z-0 w-full mb-6 group">
-        <input type="tel" onChange={handleChange} pattern="^\+62\d{9,15}$" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <input type="tel" onChange={handleChange} onInput={formatPhoneNumber} pattern="^\+62\d{9,15}$" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
         <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (+6212008867534)</label>
         </div>
         </div>
